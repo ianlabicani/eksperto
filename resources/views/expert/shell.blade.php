a2ae835755c425d8ad969baede909ab0ce104823
ianlabicani <labicaniian@gmail.com>
2025-03-19 08:43:58 +0800

    use local resources instead of cdn

--- resources/views/expert/shell.blade.php
+++ resources/views/expert/shell.blade.php
@@ -12,14 +12,14 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet">
 
-    <!-- Scripts -->
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
 
     <!-- Page Content -->
-    @include('client.ui.navbar')
+    @include('expert.ui.navbar')
     <main class="flex-grow-1">
         @yield('content')
     </main>
