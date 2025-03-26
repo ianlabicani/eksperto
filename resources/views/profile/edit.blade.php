6c7ca27c7870e7dc8e857b0ffda5d54d12a64c49
ianlabicani <labicaniian@gmail.com>
2025-03-26 11:51:29 +0800

    add note for finished profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -15,6 +15,10 @@
                     <div class="alert alert-warning mt-3">
                         <i class="fas fa-exclamation-triangle"></i> Please complete your profile to access all features.
                     </div>
+                @else
+                    <div class="alert alert-success mt-3">
+                        <i class="fas fa-check-circle"></i> Your profile is complete. You can now access all features.
+                    </div>
                 @endif
             </div>
         </div>
